47e66632a6f64db9d80db874d3539042e4f6b1f4
[dyad] <kanaraguts@gmail.com>
2026-06-15 18:01:15 +0600

    Updated src/content/pickupLines.ts

--- src/content/pickupLines.ts
+++ src/content/pickupLines.ts
@@ -1,7 +1,7 @@
 import { PickupLine } from "@/types";
 
 export const pickupLines: PickupLine[] = [
-  "Are you a magician? Because whenever I look at you, everyone else disappears.",
+  "Are your parents pirats? Then why are you such a treasure",
   "Do you have a map? I just got lost in your eyes.",
   "Is your name Google? Because you have everything I’m searching for.",
   // Add more lines here easily...
